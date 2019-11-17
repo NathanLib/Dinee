54f311eb78ab38876ee54ed341eb15d84319103b
Nathan <nathan.lib19@gmail.com>
2019-11-17 19:31:14 +0000

    Sign in and Sign up pages done

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+function addEvent(element, type, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(type, listener, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + type, listener);
+    }
+}
+
+// *****************************************
+
+jQuery.fn.center = function () {
+    this.css("position", "absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+        $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+        $(window).scrollLeft()) + "px");
+    return this;
+}
+
+// *****************************************
+
+function log_form_center() {
+    $('.containerLog').center();
+}
+
+addEvent(window, "load", log_form_center);
+addEvent(window, "resize", log_form_center);
+
+// *****************************************
+
+$(document).ready(function () {
+
+});
